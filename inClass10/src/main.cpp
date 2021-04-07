4816575c7406652317b1e803d35f72045e325762
karl <karl.statz@gmail.com>
2021-04-07 18:01:39 -0500

    InClass10 Assignment

---
+++ inClass10/src/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+int Divide(int dividend, int divisor){
+    bool isWhole = true;
+    int counter = 0;
+    while(isWhole){
+        if(divisor > 0){
+            dividend -= divisor;
+            if(dividend >= 0){
+                counter++;
+            }
+            else{
+                return counter;
+            }
+        }else if (divisor < 0){
+            dividend += divisor;
+            if(dividend >= 0){
+                counter++;
+            }
+            else{
+                return counter*-1;
+            }
+        }
+    }
+    return counter;
+}
+
+int main(){
+    int dividend, divisor;
+    std::cout << "Enter Dividend: ";
+    std::cin >> dividend;
+    std::cout << "Enter Divisor: ";
+    std::cin >> divisor;
+
+    int result = Divide(dividend, divisor);
+    std::cout << result << std::endl;
+}
